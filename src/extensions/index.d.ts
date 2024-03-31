d7885f81c801ce446e90f5daf3e878ee811d90e9
Muhammad Swalah A A <swalah@heedlabs.com>
2024-03-31 12:05:58 +0530

    fix: express extension index.d.ts file

---
+++ src/extensions/index.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable import/export */
+/**
+ * @file index.d.ts
+ * @description Exports all custom types
+ * @module index.d.ts
+ *
+ * All custom extension types are exported from this file.
+ */
+
+export * from "./express";
